28cc9fb60d753cef40bc4e8ceecc1faa38a612ae
manTTale <mantale.roberto1@yahoo.com>
2023-06-26 10:10:53 +0300

    first step to get the categoryId from the REST API and not hardcode it

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });    
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+    //check if id param is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(!hasCategoryId) {
+      //get id and convert to string using +
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else{
+      //not category id found, we set default to 1
+      this.currentCategoryId=1;
+    }
+
+    //get the products for the given id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
