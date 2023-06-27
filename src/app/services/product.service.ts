4d134abe00e734ca32e41c11fbc16e8357dcfad6
manTTale <mantale.roberto1@yahoo.com>
2023-06-27 18:47:35 +0300

    implemented the getProduct method and changed the html

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,9 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  getProduct(theProductId: number) {
-    throw new Error('Method not implemented.');
-  }
+
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
@@ -42,6 +40,14 @@ export class ProductService {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
+  }
+
+  getProduct(theProductId: number): Observable<Product> {
+    //build url based on the product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+
   }
 }
 
