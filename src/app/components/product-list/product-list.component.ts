5988c1b18a9bd79daa173e71bbca4ca1e85dc0c3
manTTale <mantale.roberto1@yahoo.com>
2023-07-07 14:37:52 +0300

    forgot to add this commit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -23,6 +23,8 @@ export class ProductListComponent implements OnInit {
   thePageSize: number = 5;
   theTotalElements: number = 0;
 
+  previousKeyword: string=null;
+
 
 
   constructor(private productService: ProductService,
@@ -49,11 +51,21 @@ export class ProductListComponent implements OnInit {
   handleSearchtProducts() {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
-    this.productService.searchProducts(theKeyword).subscribe(
-      (data: Product[]) => {
-        this.products = data;
-      }
-    )
+    //if we have different keyword than prev, set page number to 1
+    if (this.previousKeyword != theKeyword){
+      this.thePageNumber=1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+
+    //search using keyword
+    this.productService.searchProductsPaginate(this.thePageNumber-1,
+                                               this.thePageSize,
+                                               theKeyword).subscribe(this.processResult());
+                                               
   }
 
   handleListProducts() {
